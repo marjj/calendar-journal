b2d1083d56e1e9dd020d5d92d5fdf2bc450c4ac1
Marjo Sobrecaray <marjo.sobrecaray@gmail.com>
2025-10-01 21:29:00 +0800

    added tailwind, components and dummy data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 
+import Card from './components/Card';
+import Calendar from './components/Calendar';
+
+import data from './data'
+
 function App() {
   return (
-    <div className="app-container">
-      <div className="card">1</div>
-      <div className="card">2 something went wrong</div>
+    <div className="app-container grid grid-rows-[40%_1fr] md:grid-rows-1 md:grid-cols-[2fr_1fr] gap-4 p-4 h-[100vh] overflow-auto md:overflow-hidden">
+      <Calendar />
+      <div className="md:max-h-full md:overflow-y-scroll">
+        <h2 className="text-lg font-bold mb-4">Notes</h2>
+        { data.data.map((d, i) => <Card key={i} title={d.title} content={d.content} />) }
+      </div>
     </div>
   );
 }
---
+++ src/data/index.js
@@ -0,0 +1,66 @@
+const test = {
+    data: [
+        {
+            date: "2025-09-29-2025",
+            title: "Card Title",
+            content: "lorem ipsum lasldsad dasdasdasdasdasd"
+        },
+        {
+            date: "2025-09-29-2025",
+            title: "Card Title",
+            content: "lorem ipsum lasldsad dasdasdasdasdasd"
+        },
+        {
+            date: "2025-09-29-2025",
+            title: "Card Title",
+            content: "lorem ipsum lasldsad dasdasdasdasdasd"
+        },
+        {
+            date: "2025-09-29-2025",
+            title: "Card Title",
+            content: "lorem ipsum lasldsad dasdasdasdasdasd"
+        },
+        {
+            date: "2025-09-29-2025",
+            title: "Card Title",
+            content: "lorem ipsum lasldsad dasdasdasdasdasd"
+        },
+        {
+            date: "2025-09-29-2025",
+            title: "Card Title",
+            content: "lorem ipsum lasldsad dasdasdasdasdasd"
+        },
+        {
+            date: "2025-09-29-2025",
+            title: "Card Title",
+            content: "lorem ipsum lasldsad dasdasdasdasdasd"
+        },
+        {
+            date: "2025-09-29-2025",
+            title: "Card Title",
+            content: "lorem ipsum lasldsad dasdasdasdasdasd"
+        },
+        {
+            date: "2025-09-29-2025",
+            title: "Card Title",
+            content: "lorem ipsum lasldsad dasdasdasdasdasd"
+        },
+        {
+            date: "2025-09-29-2025",
+            title: "Card Title",
+            content: "lorem ipsum lasldsad dasdasdasdasdasd"
+        },
+        {
+            date: "2025-09-29-2025",
+            title: "Card Title",
+            content: "lorem ipsum lasldsad dasdasdasdasdasd"
+        },
+        {
+            date: "2025-09-29-2025",
+            title: "Card Title",
+            content: "lorem ipsum lasldsad dasdasdasdasdasd"
+        },
+    ]
+}
+
+export default test;
